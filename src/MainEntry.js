aafb74de25090b68875983b6a1c0ea4cad140f52
salboaie <salboaie@gmail.com>
2012-05-02 00:59:52 +0300

    redis added,half working already

--- src/MainEntry.js
+++ src/MainEntry.js
@@ -10,13 +10,39 @@
 */
 
 var journey = require('journey');
+var redis = require("redis"),
+    client = redis.createClient(6379);
 
-function putMessage(req, res, data) {
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+
+function putMessage(req, res,roomId, data) {
     console.log(data);
     res.send(200);
 }
 
+function addMessage(req, res, roomId, userName, message) {
+    var msg= {room:roomId, user:userName,date:null,message:message};
+    console.log(JSON.stringify(msg));
+
+    client.lpush(roomId,JSON.stringify(msg),redis.print);
+    res.send(200, {},msg);
+}
+
+
 function getPage(req, res, roomId, pageNumber, pageLines) {
+
+    //client.lrange(roomId,pageNumber*pageLines,(pageNumber+1)*pageLines,redis.print);
+
+    client.lrange(roomId,pageNumber*pageLines,(pageNumber+1)*pageLines,function (err, replies){
+            console.log(replies.length + " replies:");
+            replies.forEach(function (reply, i) {
+                console.log("    " + i + ": " + reply);
+            });
+        }
+    );
     res.send(200, {}, {room:roomId, page:pageNumber,lines:pageLines});
     //res.send('getPage: ' + req.params[0] + " PageNumber: " + req.params[1] + " PageCount: " + req.params[2]);
 }
@@ -35,6 +61,7 @@ var router = new(journey.Router);
 router.map(function () {
     this.root.bind(function (req, res) { res.send("Welcome") });
     this.get(/^\/(.+)\/([0-9]+)\/([0-9]+)/).bind(getPage);
+    this.get(/^add\/(.+)\/(.+)\/(.*)/).bind(addMessage);
     this.put(/^\/(.+)\/message/).bind(putMessage);
     this.get(/^\/(.+)\/count/).bind(count);
 
